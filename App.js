7c5c2c3301bfa9714fb06f9b0864bf928dda835f
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-05-06 23:43:25 +0630

    scroll view

--- App.js
+++ App.js
@@ -1,27 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { ScrollView, Text, View,Image, TextInput } from 'react-native';
 export default function App() {
 
-  const name = "Maru";
-  const getFullName = (firstName,secondName,thirdName) => {
-    return firstName + ' ' + secondName +  ' ' + thirdName
-  }
+  // const name = "Maru";
+  // const getFullName = (firstName,secondName,thirdName) => {
+  //   return firstName + ' ' + secondName +  ' ' + thirdName
+  // }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! {name}!</Text>
-      <Text>Hello I am {getFullName('Hein','Wai','Yan')}</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <ScrollView>
+
+         <Text>Some Text</Text>
+         <View>
+
+            <Text>Some more Text</Text>
+
+            <Image
+              source={{
+                uri:'https://reactnative.dev/docs/assets/p_cat2.png'
+              }}
+              style={{width:200,height:200}}
+            >
+            </Image>
+            
+         </View>
+
+         <TextInput
+            style={{
+              height:40,
+              borderColor:'gray',
+              borderWidth:1
+            }}
+            defaultValue="You can type in me"
+         >
+         </TextInput>
+
+    </ScrollView>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app! {name}!</Text>
+    //   <Text>Hello I am {getFullName('Hein','Wai','Yan')}</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
