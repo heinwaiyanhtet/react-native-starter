d796f063f6814429ec0ae3189981abce35d318b9
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-05-06 23:27:09 +0630

    core components

--- App.js
+++ App.js
@@ -2,9 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const name = "Maru";
+  const getFullName = (firstName,secondName,thirdName) => {
+    return firstName + ' ' + secondName +  ' ' + thirdName
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! </Text>
+      <Text>Open up App.js to start working on your app! {name}!</Text>
+      <Text>Hello I am {getFullName('Hein','Wai','Yan')}</Text>
       <StatusBar style="auto" />
     </View>
   );
